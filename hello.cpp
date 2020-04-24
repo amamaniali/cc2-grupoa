7f9419c40974c2010aa6ff94ab98123b1ada63cb
Alvaro Mamani Aliaga <amamaniali@unsa.edu.pe>
2020-04-24 09:32:23 -0500

    Cambios en el archivo hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -3,6 +3,7 @@
 int main () {
 	std::cout << "Bienvenidos al curso de Ciencia de la Computacion II" << std::endl;
 	std::cout << "Todo va a estar bien!!!!!" << std::endl;
+	std::cout << "Si estudias, obvio!!!!" << std::endl;
 
 	return 0;
 }
