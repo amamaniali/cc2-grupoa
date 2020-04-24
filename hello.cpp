63e97f858c4a3886779f1ad27f80c9bf90151673
Alvaro Mamani Aliaga <amamaniali@unsa.edu.pe>
2020-04-24 09:29:26 -0500

    Primer programa

---
+++ hello.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main () {
+	std::cout << "Bienvenidos al curso de Ciencia de la Computacion II" << std::endl;
+	std::cout << "Todo va a estar bien!!!!!" << std::endl;
+
+	return 0;
+}
+
